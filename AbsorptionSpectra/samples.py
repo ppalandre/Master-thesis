c6a3fdaa39765877ecf8c41ab8998b0b9ef61d16
Pauline Palandre <popauline57@gmail.com>
2024-05-03 16:28:30 +0200

    added file with dict of samples and how they should be plotted (color, label, linestyle) to avoid writing one script for plotting different experiments but rather always use the same functions

---
+++ AbsorptionSpectra/samples.py
@@ -0,0 +1,24 @@
+# all samples used in my absorption spectra experiments mapped to the color and label they should have
+
+samples = {"WT": {"color": "black", "label": "WT", "linestyle": "solid"},
+           "DualTag": {"color": "grey", "label": "GFP", "linestyle": "solid"},
+           "delta-rbp1 #2": {"color": "#c43e96", "label": r"$\Delta \it{rbp1}$ #2", "linestyle": "dashed"},
+           "delta-rbp1 #4": {"color": "#c43e96", "label": r"$\Delta \it{rbp1}$", "linestyle": "dashed"},
+           "delta-rbp1 #6": {"color": "#c43e96", "label": r"$\Delta \it{rbp1}$", "linestyle": "dashed"},
+           "delta-rbp1": {"color": "#5aaa46", "label": r"$\Delta \it{rbp1}$", "linestyle": "dashed"},
+           "RBP1-GFP #4": {"color": "#5aaa46", "label": "Rbp1-GFP", "linestyle": "dotted"},
+           "RBP1-GFP #11": {"color": "#5aaa46", "label": "Rbp1-GFP #11", "linestyle": "dotted"},
+           "RBP1-GFP #12": {"color": "#5cb56f", "label": "Rbp1-GFP #12", "linestyle": "dotted"},
+           "RBP1-GFP": {"color": "#5cb56f", "label": "Rbp1-GFP", "linestyle": "dotted"},
+           "RBP1-FLAG #6.6": {"color": "#317ec2", "label": "Rbp1-FLAG", "linestyle": "dotted"},
+           "RBP1-FLAG": {"color": "#317ec2", "label": "Rbp1-FLAG", "linestyle": "dotted"}
+           }
+
+
+# colors when biological replicates are plotted together
+#"RBP1-GFP #4": {"color": "#0c8b30", "label": "Rbp1-GFP", "linestyle": "dotted"},
+#"RBP1-GFP #11": {"color": "#124c02", "label": "Rbp1-GFP #11", "linestyle": "dotted"},
+#"RBP1-GFP #12": {"color": "#5cb56f", "label": "Rbp1-GFP #12", "linestyle": "dotted"},
+#"delta-rbp1 #2": {"color": "#d952e7", "label": r"$\Delta \it{rbp1}$ #2", "linestyle": "dashed"},
+#"delta-rbp1 #4": {"color": "#ff00d0", "label": r"$\Delta \it{rbp1}$", "linestyle": "dashed"},
+#"delta-rbp1 #6": {"color": "#a11b65", "label": r"$\Delta \it{rbp1}$", "linestyle": "dashed"},
